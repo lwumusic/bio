05f3b4a58fa1d22f9500ed8b317ddb1c4f2c2323
lwu877 <lex@lexwu.com>
2025-12-30 03:05:40 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   theme: {
     fontFamily: {
       display: ['Figtree Variable', 'sans-serif'],
-      sans: ['Inter Variable', 'sans-serif'],
+      sans: ['Geist Variable', 'Inter Variable', 'sans-serif'],
     },
   },
 }
